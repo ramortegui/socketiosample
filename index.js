048302c6b370e0d130cbcc8f962a409232c742b5
Ruben Amortegui <ruben.amortegui@gmail.com>
2016-07-09 20:02:07 -0600

    Adding socket.io, and emiting and receiving events on server.

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 var app = require('express')();
 var http = require('http').Server(app);
-
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-	res.send('<h1>Hello World</h1>');
+	res.sendFile( __dirname + '/index.html' );
 });
 
+
+io.on('connection', function(socket){
+	console.log('Receiving a new connection');
+
+	socket.on('chat message', function(msg) {
+		console.log('message: '+msg);
+	});
+
+	socket.on('disconnect', function(){
+		console.log('connection lost.');
+	});
+});
 
 http.listen(3000, function(){
 	console.log("Server listening on port 3000");
