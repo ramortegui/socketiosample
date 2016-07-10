a81f4685ea079ab0bd073778c2f2607ca125c518
Ruben Amortegui <ruben.amortegui@gmail.com>
2016-07-09 19:39:54 -0600

    Initial commit.  Express app running.

---
+++ index.js
@@ -0,0 +1,12 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+
+app.get('/', function(req, res){
+	res.send('<h1>Hello World</h1>');
+});
+
+
+http.listen(3000, function(){
+	console.log("Server listening on port 3000");
+});
